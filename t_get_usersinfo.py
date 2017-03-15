ae69f997cd9193409d50c2b3ec1efea637dcd534
tauwong <huangtaosunflower@gmail.com>
2017-03-15 16:46:02 +0800

    Create t_get_usersinfo.py

---
+++ t_get_usersinfo.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python
+# -*- coding: UTF-8 -*-
+# get common user's informations which obtain username, home dir, shell from file '/etc/passwd'
+
+def main():
+    backup_file = 'userinfo.txt'
+    with open('/etc/passwd') as source, open(backup_file, 'w') as target:
+        for line in source:
+            data = line.split(':')
+            user, home, shell = data[0], data[5], data[6].strip
+            target.write("{0},{1},{2}\n".format(user, home, shell)
+            
+            
+ if __name__ == '__main__':
+     main()
