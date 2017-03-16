cc669c380e6d4bbe8ddb36145ce310bb910a619e
tauwong <huangtaosunflower@gmail.com>
2017-03-16 15:01:48 +0800

    Create t_deploy_mongodb.py
    deploy MongoDB using gzip-format file on Linux

---
+++ t_deploy_mongodb.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python
+# -*- coding: UTF-8 -*-
+# deploy MongoDB using gzip-format file on Linux.
+
+
+import os
+import shutil
+import tarfile
+
+
+def prepare_env(data_dir, package):
+    package_dir = os.path.splitext(package)[0]
+    if os.path.exists(package_dir):
+        shutil.rmtree(package_dir)
+    if os.path.exists(data_dir):
+        shutil.rmtree(data_dir)
+    os.mkdir(data_dir)
+    # tar mongo package
+    t = tarfile.open(package, 'r')
+    t.extractall('.')
+
+
+def main():
+    data_dir = os.path.join(os.path.expanduser('~'), 'mongodata')
+    package = 'mongodb-linux-x86_64-rhel70-3.4.2.tgz'
+    if not os.path.exists(package):
+        raise SystemExit("{0} not found".format(package))
+    package_dir = os.path.splitext(package)[0]
+    mongod = os.path.join(os.path.realpath('.'), package_dir, 'bin', 'mongod')
+    os.system("{0} --dbpath {1} &".format(mongod, data_dir))
+
+
+if __name__ == '__main__':
+    main()
